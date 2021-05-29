929b6e5787bba84eb933476b6bb6468283a8ea3c
Momsy <66912365+Momsy@users.noreply.github.com>
2021-05-29 14:37:15 +0200

    Delete Esiea3A-master directory

--- Esiea3A-master/app/src/main/java/com/example/esiea3a/presentation/list/PokemonModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.esiea3a.presentation.list
-
-
-sealed class PokemonModel
-
-data class PokemonSuccess(val pokeList: List<Pokemon>) : PokemonModel()
-object PokemonLoader : PokemonModel()
-object PokemonError : PokemonModel()
